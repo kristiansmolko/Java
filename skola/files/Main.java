d7228805272f2931c431f3a198e9f70b64ad1136
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2020-11-11 09:37:31 +0100

    Added resource text and file

---
+++ skola/files/Main.java
@@ -0,0 +1,8 @@
+package skola.files;
+
+public class Main {
+    public static void main(String[] args) {
+        ReadFromFileLine rwFile = new ReadFromFileLine();
+        rwFile.copyFileWithCapital("resource/a.txt");
+    }
+}
